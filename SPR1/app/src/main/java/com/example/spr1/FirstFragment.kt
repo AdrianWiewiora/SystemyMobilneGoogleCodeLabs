11ebc3a5d7efd7151f04f227b0e44227c205186d
Adrian Wiewióra <93733010+AdrianWiewiora@users.noreply.github.com>
2023-10-23 09:50:16 +0200

    SPR1 8

--- SPR1/app/src/main/java/com/example/spr1/FirstFragment.kt
+++ SPR1/app/src/main/java/com/example/spr1/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.spr1.databinding.FragmentFirstBinding
 
@@ -35,6 +37,25 @@ class FirstFragment : Fragment() {
         binding.randomButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        binding.toastButton.setOnClickListener {
+            val myToast = Toast.makeText(context, "Hello Toast", Toast.LENGTH_SHORT)
+            myToast.show()
+        }
+
+        binding.countButton.setOnClickListener {
+            countMe(view)
+        }
+
+    }
+
+
+    private fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+        var count = countString.toInt()
+        count++
+        showCountTextView.text = count.toString()
     }
 
     override fun onDestroyView() {
