d4eb77d501fe3c511387cb52360699d135ad13aa
Adrian Wiewióra <93733010+AdrianWiewiora@users.noreply.github.com>
2023-11-27 19:16:59 +0100

    SPR 3.5

--- SPR3/app/src/main/java/com/example/spr3/MainActivity.kt
+++ SPR3/app/src/main/java/com/example/spr3/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.spr3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spr3.adapter.ItemAdapter
+import com.example.spr3.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
+
